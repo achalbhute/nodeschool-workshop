13bc66b13845565ff3756949dba89d6c9a81f541
Achal Bhute <achal_bhute@persistent.co.in>
2017-09-06 19:12:32 +0530

    3rd

--- pug.js
+++ pug.js
@@ -1,15 +1,11 @@
 var express = require('express');
-var path = require('path');
-var pug = require ('pug');
 var app = express();
 
 app.set('view engine', 'pug');
+app.set('views', process.argv[3]);
 
 app.get('/home',function(req, res){
-    res.render(process.argv[3], {date :new Date().toDateString()})
+    res.render('index', {date :new Date().toDateString()})
 })
-
-//app.set('views', path.join(__dirname, process.argv[3]));
-
 
 app.listen(process.argv[2]);
